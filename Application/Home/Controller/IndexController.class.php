77918ed9ea0ae5e41b4ee29e56470b26a010f134
hainuo <admin@hainuo.info>
2014-05-20 17:43:20 +0800

    fix
    fixurl rewrite

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -20,7 +20,7 @@ class IndexController extends Controller {
 	public function getMember() {
         $userName=I('request.username');
         if(preg_match('/\?.*=/',__SELF__,$url)){
-            $url="/getMember/username/".$userName;
+            $url="/index/getMember/username/".$userName;
             $this->redirect($url);
         }
 		if($userName){
@@ -51,7 +51,7 @@ class IndexController extends Controller {
     public function getWeight(){
         $userName=I('request.username');        
         if(preg_match('/\?.*=/',__SELF__,$url)){
-            $url="/getWeight/username/".$userName;
+            $url="/index/getWeight/username/".$userName;
             $this->redirect($url);
         }
         $page=I('get.page');
