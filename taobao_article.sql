8740edd1a5eb31159f31c94ab5a9d5d90f8e0fbd
hainuo <admin@hainuo.info>
2014-05-27 20:49:36 +0800

    增加umeditor
    增加编辑器

---
+++ taobao_article.sql
@@ -0,0 +1,32 @@
+﻿# SQL-Front 5.1  (Build 4.16)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+
+
+# Host: localhost    Database: taobao
+# ------------------------------------------------------
+# Server version 5.5.36
+
+#
+# Source for table taobao_article
+#
+
+DROP TABLE IF EXISTS `taobao_article`;
+CREATE TABLE `taobao_article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Dumping data for table taobao_article
+#
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
