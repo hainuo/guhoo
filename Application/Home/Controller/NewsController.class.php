f0e01872ea3f9721924d5c40c0192eab7398396e
hainuo <admin@hainuo.info>
2014-05-28 11:34:21 +0800

    更改了前台新闻模块的样式
    增加了新闻模块调用seo插件
    增加了新闻模块调用统计插件
    增加了新闻模块详情页面前一篇，后一篇的调用

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -17,22 +17,62 @@ class NewsController extends Controller{
         $id=I('get.id');
         $artModel=D('Article');
         if($id){
-            $dataList=$artModel->order('create_time DESC')->select();
-            if($dataList){
-                $this->assign('dataLists',$dataList);
-                $this->assign('id',$id);
-                $this->seo();
-                $this->display();
+                $dataList=$artModel->order('create_time DESC')->select();
+                if($dataList){
+                    foreach($dataList as $k=>$v){
+                        $ids[]=$v['id'];
+                        if($k==0) $last=$v;
+                        if($id==$v['id']){
+                            $previousData=$dataList[$k-1];
+                            $data=$v;
+                            $nextData=$dataList[$k+1];
+                        }
+                    }
+                    $this->assign('data',$data);
+                    $this->assign('last',$last);
+                    $this->assign('dataLists',$dataList);
+                    $this->assign('nextData',$nextData);
+                    $this->assign('previousData',$previousData);
+                    $this->assign('count',$this->getTotalCount());
+                    $this->seo();
+                    $this->display();
             }else
                 $this->error('您的操作有误！');
         }else
-            $this->error('您的操作有误！');;
-
+            $this->error('您的操作有误！');
     }
     public function seo()
     {
         $this->assign('title', C(ACTION_NAME . '_title'));
         $this->assign('keywords', C(ACTION_NAME . '_keyWords'));
         $this->assign('description', C(ACTION_NAME . '_description'));
+    }
+    /**
+     * NewsController::getTotalCount()
+     * //得到查询的总次数
+     * TODO  本方法没有判断是否查询，不管是否查询，只要调用本方法，查询次数会自动加1
+     * @return
+     */
+    public function getTotalCount()
+    {
+        //获取查询taobao帐户的总次数
+        $nuberModel = D('Number');
+        $count = $nuberModel->where('name="taobao"')->find();
+        //dump($count);
+        //trace(serialize($count),'总次数数据');
+        $count['val'] += rand(1,5);
+        //var_dump($count['val']);
+        $nuberModel->save($count);
+        //return $count['val'];
+        $code = '';
+        $val = str_split($count['val']);
+        $val=array_reverse($val);
+        $length=count($val);
+        foreach ($val as $k => $v) {
+            $code = '<i class="numbers-i wh n' . $v . '"></i>'.$code ;
+            if (($k+1) % 3 == 0 && ($k+1)!=$length)
+                $code = '<img src="/Public/images/Comma.png" />'.$code;
+        }
+        return $code;
     }
 } 
--- taobao_article.sql
+++ taobao_article.sql
@@ -20,13 +20,17 @@ CREATE TABLE `taobao_article` (
   `title` varchar(255) DEFAULT NULL,
   `content` text,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据创建时间',
+  `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 #
 # Dumping data for table taobao_article
 #
 
+INSERT INTO `taobao_article` VALUES (1,'范德萨分','&lt;p&gt;范德萨分是&lt;/p&gt;&lt;p&gt;法师打飞机啊圣诞快乐福建可拉萨的减肥 是否大家看法来看 萨达夫金克拉&lt;/p&gt;','2014-05-27 20:02:38','admin');
+INSERT INTO `taobao_article` VALUES (2,'发生大法师的','<p>防守打法萨达夫仨</p>','2014-05-27 15:43:07','admin');
+INSERT INTO `taobao_article` VALUES (5,'粉兔兔网站欢迎你','&lt;p&gt;粉兔兔网站欢迎你&lt;/p&gt;','2014-05-27 20:30:32','admin');
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
